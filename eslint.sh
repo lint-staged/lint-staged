05f8f6e13731b98bc78a69fac3cd99d10aaeedbf
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-01-15 12:07:52 +0100

    Better glob patterns

--- eslint.sh
+++ eslint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/.bin/staged-files '*.@(js|jsx)' -- node_modules/.bin/eslint
+node_modules/.bin/staged-files '**/*.@(js|jsx)' -- node_modules/.bin/eslint
--- stylelint.sh
+++ stylelint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-node_modules/.bin/staged-files '*.@(css|scss|less|styl)' -- node_modules/.bin/stylelint
+node_modules/.bin/staged-files '**/*.@(css|scss|less|styl)' -- node_modules/.bin/stylelint
