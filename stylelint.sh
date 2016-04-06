b09456fcf6b222eeaa91d2482b8a44ad48e57df0
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-04-06 19:20:37 +0200

    Check if the executable for linter exist and warn (with instructions) if not. Closes #2

--- stylelint.sh
+++ stylelint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-node_modules/.bin/staged-files '**/*.@(css|scss|less|styl)' -- node_modules/.bin/stylelint
+executable=node_modules/.bin/staged-files
+linter_name="stylelint"
+linter_path=./node_modules/.bin/stylelint
+lint_extensions="**/*.@(css|scss|less|styl)"
+
+if [[ -e "${linter_path}" ]]; then
+  echo "Running ${linter_name} on git staged files: ${lint_extensions}"
+  ${executable} "${lint_extensions}" -- ${linter_path}
+else
+  echo "Could not find ${linter_name} at ${linter_path}. Is it installed?"
+  echo ""
+  echo "Try running:"
+  echo "npm install --save-dev ${linter_name}"
+fi
