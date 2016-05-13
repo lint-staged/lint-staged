bcae7dc27fa7fad42a76d3e2a044d27bdabb4e7a
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-05-13 21:20:14 +0200

    Experimenting with VinylFS and gulp plugins

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var npmRun = require('npm-run');
 var spawn = require("gulp-spawn")
 var filter = require("gulp-filter")
 var eslint = require('gulp-eslint')
-
+var fs = require('vinyl-fs');
 
 var log = function(file, cb) {
     console.log(JSON.stringify(file));
@@ -13,8 +13,10 @@ var log = function(file, cb) {
 
 stagedFiles()
     // .pipe(filter(['*.js']))
+    // .pipe(fs.src(['**/*.js'], { passthrough: true }))
     // .pipe(map(log))
-    .pipe(eslint())
+    .pipe(eslint({
+        useEslintrc: true
+    }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
-    // .pipe(log)
