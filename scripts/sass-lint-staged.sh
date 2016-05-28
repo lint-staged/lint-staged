27efc2ffa2963b19e23cc96ec986528798d1688a
Remo Vetere <remo.vetere@gmail.com>
2016-05-29 01:13:52 +0200

    Add support for sass-lint

---
+++ scripts/sass-lint-staged.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+executable=$(npm bin)/staged-files
+linter_name="sass-lint"
+linter_path=$(npm bin)/sass-lint
+lint_extensions="**/*.@(scss|scs)"
+
+if [[ -f "${linter_path}" ]]; then
+  echo "Running ${linter_name} on git staged files: ${lint_extensions}"
+  ${executable} "${lint_extensions}" -- ${linter_path}
+else
+  echo "Could not find ${linter_name} at ${linter_path}. Is it installed?"
+  echo ""
+  echo "Try running:"
+  echo "npm install --save-dev ${linter_name}"
+fi
