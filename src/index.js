da0ffe14835b023ee741a7eecf72166e4f328bb6
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-06-02 23:02:35 +0200

    Working proof of concept with streams

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+var spawn = require('child_process').spawn;
 var stagedFiles = require('staged-files');
-var shell = require('gulp-shell');
-var filter = require('gulp-filter');
-var eslintFilter = filter('**/*.js', { restore: true });
+var minimatch = require('minimatch');
 
-stagedFiles()
-    .pipe(eslintFilter)
-    .pipe(shell([
-        'echo Linting <%= file.path %>...',
-        'npm run --loglevel=silent eslint -- <%= file.path %> --color'
-    ]));
+var files = [];
+var linters = {
+    'eslint': '**/*.js',
+    'stylelint': '**/*.scss'
+};
+var stream = stagedFiles(null, ['--diff-filter=ACM']);
+
+function runLinter(linter, paths) {
+    var args = ['run', '-s', linter + '-staged', '--'].concat(paths);
+    var npmStream = spawn('npm', args, {
+        stdio: 'inherit' // <== IMPORTANT: use this option to inherit the parent's environment
+    });
+    npmStream.on('error', function(error) {
+        console.log('error', error);
+    });
+    npmStream.on('close', function(code) {
+        process.exitCode = code;
+    });
+}
+
+stream.on('data', function(data) {
+    files.push(data);
+});
+
+stream.on('end', function() {
+    var paths = files.map(function(file) {
+        return file.path;
+    });
+    console.log('Found %s staged files', paths.length);
+    if (paths.length) {
+        console.log('Running linters...');
+        Object.keys(linters).forEach(function(linter) {
+            var fileList = paths.filter(minimatch.filter(linters[linter], { matchBase: true }));
+            runLinter(linter, fileList);
+        });
+    }
+});
+
