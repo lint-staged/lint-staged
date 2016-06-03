a74fecf46a92c2e67ca09453588ebcf9a02d4f38
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-06-03 17:17:22 +0200

    Added spinner!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var spawn = require('child_process').spawn;
 var sgf = require('staged-git-files');
 var minimatch = require('minimatch');
+var ora = require('ora');
 
 var linters = {
     'eslint': '**/*.js',
     'stylelint': '**/*.scss'
 };
+
+var spinner = ora('Starting lint-staged').start();
 
 function runLinter(linter, paths) {
     var args = ['run', '-s', linter + '-staged', '--'].concat(paths);
@@ -16,22 +19,23 @@ function runLinter(linter, paths) {
         console.log('error', error);
     });
     npmStream.on('close', function(code) {
+        spinner.stop();
         process.exitCode = code;
     });
 }
 
 sgf('ACM', function(err, results) {
-    console.log(results);
-    var paths = results.map(function(file) {
+    if (err) {
+        console.error(err);
+    }
+    var filePaths = results.map(function(file) {
         return file.filename;
     });
-    console.log('Found %s staged files', paths.length);
-    if (paths.length) {
-        console.log('Running linters...');
+    if (filePaths.length) {
         Object.keys(linters).forEach(function(linter) {
-            var fileList = paths.filter(minimatch.filter(linters[linter], { matchBase: true }));
+            var fileList = filePaths.filter(minimatch.filter(linters[linter], { matchBase: true }));
             if (fileList.length) {
-                console.log('Running %s on %s', linter, fileList);
+                spinner.text = 'Running ' + linter + ' on ' + fileList;
                 runLinter(linter, fileList);
             }
         });
