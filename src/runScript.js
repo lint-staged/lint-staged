90cffaa4b063a4cc19fd564685736b8d990ef5ee
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-07-22 14:04:41 +0200

    Switched to Listr. Use ES2015 syntax.

--- src/runScript.js
+++ src/runScript.js
@@ -1,40 +1,35 @@
+'use strict'
+
 var findBin = require('./findBin')
-var spawn = require('child_process').spawn
-var batch = require('batchflow')
+const execa = require('execa')
 
-module.exports = function runScript (linters, paths, config, cb) {
-    var lintersArray = Array.isArray(linters) ? linters : [linters]
-    var exitCode = 0
-    batch(lintersArray)
-        .series()
-        .each(function (i, linter, next) {
-            // If previous process finished with non-zero code
-            // we'll stop executing the sequence
-            if (exitCode > 0) {
-                return next(exitCode)
-            }
-
-            findBin(linter, paths, config, function (err, binPath, args) {
-                if (err) {
-                    throw err
-                }
-                var npmStream = spawn(binPath, args, {
-                    stdio: 'inherit' // <== IMPORTANT: use this option to inherit the parent's environment
+module.exports = function runScript (linters, pathsToLint, config) {
+    const lintersArray = Array.isArray(linters) ? linters : [linters]
+    return lintersArray.map(linter => {
+        return {
+            title: linter,
+            task: () => {
+                return new Promise((resolve, reject) => {
+                    findBin(linter, pathsToLint, config, (err, binPath, args) => {
+                        if (err) {
+                            reject(err)
+                        }
+                        const npmStream = execa.spawn(binPath, args, {
+                            stdio: 'inherit' // <== IMPORTANT: use this option to inherit the parent's environment
+                        })
+                        npmStream.on('exit', code => {
+                            // exitCode = code
+                            if (code > 0) {
+                                reject(`${linter} found some errors. Please fix them and try committing again`)
+                            } else {
+                                resolve(`${linter} passed!`)
+                            }
+                        })
+                        // npmStream.on('close', next)
+                    })
                 })
-                npmStream.on('exit', function (code) {
-                    exitCode = code
-                })
-                npmStream.on('close', next)
-            })
-        })
-        .error(function (err) {
-            console.error(err)
-            cb.call(this, err, null)
-        })
-        .end(function () {
-            process.exitCode = exitCode
-            if (typeof cb === 'function') {
-                cb.call(this, null, exitCode)
             }
-        })
+        }
+    })
 }
+
