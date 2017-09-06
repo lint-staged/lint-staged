52428c4a7f6af147670fd377bd96842b67ee876e
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-06 22:10:40 +0700

    test(mocks/npm-which): convert native `indexOf` to `lodash/includes`

--- test/__mocks__/npm-which.js
+++ test/__mocks__/npm-which.js
@@ -1,7 +1,11 @@
+'use strict'
+
+const includes = require('lodash/includes')
+
 jest.genMockFromModule('npm-which')
 
 function sync(path) {
-  if (path.indexOf('missing') >= 0) {
+  if (includes(path, 'missing')) {
     throw new Error(`not found: ${path}`)
   }
   return path
