3fa4f6dafab4b14df8bacd768a45b45db68a09d7
Suhas Karanth <sudo-suhas@users.noreply.github.com>
2017-11-07 03:35:14 +0530

    style: Promisify staged-git-files (#325)

--- src/runAll.js
+++ src/runAll.js
@@ -3,6 +3,7 @@
 const sgf = require('staged-git-files')
 const Listr = require('listr')
 const has = require('lodash/has')
+const pify = require('pify')
 const runScript = require('./runScript')
 const generateTasks = require('./generateTasks')
 const resolveGitDir = require('./resolveGitDir')
@@ -24,44 +25,35 @@ module.exports = function runAll(scripts, config) {
   const renderer = config.renderer
   sgf.cwd = resolveGitDir(gitDir)
 
-  return new Promise((resolve, reject) => {
-    sgf('ACM', (err, files) => {
-      if (err) {
-        return reject(err)
-      }
-
-      /* files is an Object{ filename: String, status: String } */
-      const filenames = files.map(file => file.filename)
-      const tasks = generateTasks(config, filenames).map(task => ({
-        title: `Running tasks for ${task.pattern}`,
-        task: () =>
-          new Listr(runScript(task.commands, task.fileList, scripts, config), {
-            // In sub-tasks we don't want to run concurrently
-            // and we want to abort on errors
-            dateFormat: false,
-            concurrent: false,
-            exitOnError: true
-          }),
-        skip: () => {
-          if (task.fileList.length === 0) {
-            return `No staged files match ${task.pattern}`
-          }
-          return false
-        }
-      }))
-
-      if (tasks.length) {
-        return new Listr(tasks, {
+  return pify(sgf)('ACM').then(files => {
+    /* files is an Object{ filename: String, status: String } */
+    const filenames = files.map(file => file.filename)
+    const tasks = generateTasks(config, filenames).map(task => ({
+      title: `Running tasks for ${task.pattern}`,
+      task: () =>
+        new Listr(runScript(task.commands, task.fileList, scripts, config), {
+          // In sub-tasks we don't want to run concurrently
+          // and we want to abort on errors
           dateFormat: false,
-          concurrent,
-          renderer,
-          exitOnError: !concurrent // Wait for all errors when running concurrently
-        })
-          .run()
-          .then(resolve)
-          .catch(reject)
+          concurrent: false,
+          exitOnError: true
+        }),
+      skip: () => {
+        if (task.fileList.length === 0) {
+          return `No staged files match ${task.pattern}`
+        }
+        return false
       }
-      return resolve('No tasks to run.')
-    })
+    }))
+
+    if (tasks.length) {
+      return new Listr(tasks, {
+        dateFormat: false,
+        concurrent,
+        renderer,
+        exitOnError: !concurrent // Wait for all errors when running concurrently
+      }).run()
+    }
+    return 'No tasks to run.'
   })
 }
