e7307c2b2e27c512102af18642221b2954939dbc
Andrey Okonetchnikov <andrey@okonet.ru>
2017-11-28 20:49:19 +0100

    refactor: Use functions and normal modules.export instead of class

--- src/gitWorkflow.js
+++ src/gitWorkflow.js
@@ -3,32 +3,38 @@
 const path = require('path')
 const execa = require('execa')
 
-module.exports = {
-  getAbsolutePath(dir) {
-    return path.isAbsolute(dir) ? dir : path.resolve(dir)
-  },
+function getAbsolutePath(dir) {
+  return path.isAbsolute(dir) ? dir : path.resolve(dir)
+}
 
-  getCmdArgs(gitDir) {
-    if (gitDir) {
-      return ['--git-dir', this.getAbsolutePath(gitDir)]
-    }
-    return []
-  },
-
-  execGit(cmd, options) {
-    const cwd = options && options.cwd ? options.cwd : process.cwd()
-    const gitDir = options && options.gitDir
-    return execa('git', this.getCmdArgs(gitDir).concat(cmd), { cwd: this.getAbsolutePath(cwd) })
-  },
-
-  gitStashSave(options) {
-    return this.execGit(['stash', '--keep-index'], options)
-  },
-
-  gitStashPop(options) {
-    return this.execGit(['stash'], options)
-      .then(() => this.execGit(['stash', 'pop', 'stash@{1}'], options))
-      .then(() => this.execGit(['read-tree', 'stash'], options))
-      .then(() => this.execGit(['stash', 'drop'], options))
+function getCmdArgs(gitDir) {
+  if (gitDir) {
+    return ['--git-dir', getAbsolutePath(gitDir)]
   }
+  return []
+}
+
+function execGit(cmd, options) {
+  const cwd = options && options.cwd ? options.cwd : process.cwd()
+  const gitDir = options && options.gitDir
+  return execa('git', getCmdArgs(gitDir).concat(cmd), { cwd: getAbsolutePath(cwd) })
+}
+
+function gitStashSave(options) {
+  return execGit(['stash', '--keep-index'], options)
+}
+
+function gitStashPop(options) {
+  return execGit(['stash'], options)
+    .then(() => execGit(['stash', 'pop', 'stash@{1}'], options))
+    .then(() => execGit(['read-tree', 'stash'], options))
+    .then(() => execGit(['stash', 'drop'], options))
+}
+
+module.exports = {
+  getAbsolutePath,
+  getCmdArgs,
+  execGit,
+  gitStashSave,
+  gitStashPop
 }
