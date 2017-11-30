b9b693a0c18f92ae095209471361542afce89102
Andrey Okonetchnikov <andrey@okonet.ru>
2017-11-30 14:11:31 +0100

    WIP on better partially staged patch application and conflict resolution

--- src/gitWorkflow.js
+++ src/gitWorkflow.js
@@ -2,6 +2,9 @@
 
 const path = require('path')
 const execa = require('execa')
+const fsp = require('fs-promise')
+
+let patchPath
 
 function getAbsolutePath(dir) {
   return path.isAbsolute(dir) ? dir : path.resolve(dir)
@@ -25,7 +28,7 @@ function hasPartiallyStaged(options) {
     .then(res => {
       const tree = res.stdout
       if (tree) {
-        return execGit(['diff-index', '--exit-code', '--name-only', tree, '--'])
+        return execGit(['diff-index', '--exit-code', '--name-only', tree, '--'], options)
       }
       return false
     })
@@ -33,15 +36,82 @@ function hasPartiallyStaged(options) {
     .catch(() => true) // Found unstaged files
 }
 
+function generatePatch(options) {
+  const cwd = options && options.cwd ? options.cwd : process.cwd()
+  return execGit(['write-tree'], options)
+    .then(res => {
+      const tree = res.stdout
+      if (tree) {
+        return execGit(
+          [
+            'diff-index',
+            '--exit-code',
+            '--ignore-submodules',
+            '--binary',
+            '--no-color',
+            '--no-ext-diff',
+            tree,
+            '--'
+          ],
+          options
+        )
+      }
+      return Promise.resolve(null)
+    })
+    .then(() => {
+      console.log('Nothing to do...')
+    })
+    .catch(res => {
+      console.warn('Unstaged files detected.')
+      const patch = res.stdout
+      const filePath = path.join(cwd, '.lint-staged.patch')
+      console.log(`Stashing unstaged files to ${filePath}...`)
+      return fsp.writeFile(filePath, patch).then(() => filePath) // Resolve with filePath
+    })
+}
+
+function gitApply(patch, options) {
+  return execGit(['apply', '--whitespace=nowarn', patch], options)
+}
+
+function gitPopWithConflicts(options) {
+  return execGit(['stash'], options) // Stash auto-fixes
+    .then(() => execGit(['stash', 'pop', 'stash@{1}'], options)) // Apply initial stash first
+    .then(() => execGit(['read-tree', 'stash'], options)) // Merge with auto-fixes
+}
+
+function cleanup(options) {
+  console.info('Patch applied! Cleaning up...')
+  return execGit(['stash', 'drop'], options)
+    .then(() => fsp.unlink(patchPath))
+    .then(() => {
+      patchPath = null
+    })
+}
+
+function gitApplyPatch(patch, options) {
+  console.log('Applying patch...')
+  return gitApply(patch, options)
+    .then(() => cleanup(options))
+    .catch(() => {
+      console.warn('Stashed changes conflicted with hook auto-fixes! Restoring from conflicts...')
+      return gitPopWithConflicts(options).then(() => cleanup(options))
+    })
+}
+
 function gitStashSave(options) {
-  return execGit(['stash', '--keep-index'], options)
+  return generatePatch(options)
+    .then(res => {
+      patchPath = res // Save the reference to the path
+    })
+    .then(() => execGit(['stash', '--keep-index'], options))
 }
 
 function gitStashPop(options) {
-  return execGit(['stash'], options)
-    .then(() => execGit(['stash', 'pop', 'stash@{1}'], options))
-    .then(() => execGit(['read-tree', 'stash'], options))
-    .then(() => execGit(['stash', 'drop'], options))
+  if (!patchPath) {
+    throw new Error('No patch found')
+  }
+  return execGit(['checkout', '--', '.'], options).then(() => gitApplyPatch(patchPath, options))
 }
 
 module.exports = {
