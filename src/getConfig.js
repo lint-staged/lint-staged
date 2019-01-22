7506d5a83f143db9d7eda13cc16e46da93af95a7
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-01-22 21:34:30 +0100

    Improve configuration validation

--- src/getConfig.js
+++ src/getConfig.js
@@ -6,9 +6,12 @@ const chalk = require('chalk')
 const format = require('stringify-object')
 const intersection = require('lodash/intersection')
 const defaultsDeep = require('lodash/defaultsDeep')
+const isArray = require('lodash/isArray')
+const isBoolean = require('lodash/isBoolean')
+const isFunction = require('lodash/isFunction')
+const isInteger = require('lodash/isNumber')
 const isObject = require('lodash/isObject')
-const { validate, logValidationWarning } = require('jest-validate')
-const { unknownOptionWarning } = require('jest-validate/build/warnings')
+const isString = require('lodash/isString')
 const isGlob = require('is-glob')
 
 const debug = require('debug')('lint-staged:cfg')
@@ -46,26 +49,52 @@ function isSimple(config) {
   )
 }
 
+const logDeprecation = (opt, helpMsg) => `● Deprecation Warning:
+
+  Option ${chalk.bold(opt)} was removed.
+
+  ${helpMsg}
+
+  Please remove ${chalk.bold(opt)} from your configuration.
+
+Please refer to https://github.com/okonet/lint-staged#configuration for more information...`
+
+const logUnknown = (opt, helpMsg, value) => `● Validation Warning:
+
+  Unknown option ${chalk.bold(`"${opt}"`)} with value ${chalk.bold(
+  format(value, { inlineCharacterLimit: Number.POSITIVE_INFINITY })
+)} was found in the config root.
+
+  ${helpMsg}
+
+Please refer to https://github.com/okonet/lint-staged#configuration for more information...`
+
+const logError = (opt, helpMsg, value) => `● Validation Error:
+
+  Invalid value for '${chalk.bold(opt)}'.
+
+  ${helpMsg}.
+ 
+  Configured value is: ${chalk.bold(
+    format(value, { inlineCharacterLimit: Number.POSITIVE_INFINITY })
+  )}
+
+Please refer to https://github.com/okonet/lint-staged#configuration for more information...`
+
 /**
- * Custom jest-validate reporter for unknown options
+ * Reporter for unknown options
  * @param config
- * @param example
  * @param option
- * @param options
  * @returns {void}
  */
-function unknownValidationReporter(config, example, option, options) {
+function unknownValidationReporter(config, option) {
   /**
    * If the unkonwn property is a glob this is probably
    * a typical mistake of mixing simple and advanced configs
    */
   if (isGlob(option)) {
     // prettier-ignore
-    const message = `  Unknown option ${chalk.bold(`"${option}"`)} with value ${chalk.bold(
-    format(config[option], { inlineCharacterLimit: Number.POSITIVE_INFINITY })
-  )} was found in the config root.
-
-  You are probably trying to mix simple and advanced config formats. Adding
+    const message = `You are probably trying to mix simple and advanced config formats. Adding
 
   ${chalk.bold(`"linters": {
     "${option}": ${JSON.stringify(config[option])}
@@ -73,12 +102,11 @@ function unknownValidationReporter(config, example, option, options) {
 
   will fix it and remove this message.`
 
-    const { comment } = options
-    const name = options.title.warning
-    return logValidationWarning(name, message, comment)
+    return logUnknown(option, message, config[option])
   }
-  // If it is not glob pattern, use default jest-validate reporter
-  return unknownOptionWarning(config, example, option, options)
+
+  // If it is not glob pattern, simply notify of unknown value
+  return logUnknown(option, '', config[option])
 }
 
 /**
@@ -109,12 +137,6 @@ function getConfig(sourceConfig, debugMode) {
   return config
 }
 
-const optRmMsg = (opt, helpMsg) => `  Option ${chalk.bold(opt)} was removed.
-
-  ${helpMsg}
-
-  Please remove ${chalk.bold(opt)} from your configuration.`
-
 /**
  * Runs config validation. Throws error if the config is not valid.
  * @param config {Object}
@@ -122,28 +144,98 @@ const optRmMsg = (opt, helpMsg) => `  Option ${chalk.bold(opt)} was removed.
  */
 function validateConfig(config) {
   debug('Validating config')
-  const exampleConfig = {
-    ...defaultConfig,
-    linters: {
-      '*.js': ['eslint --fix', 'git add'],
-      '*.css': 'stylelint'
+
+  const errors = []
+  const warnings = []
+
+  if (!isBoolean(config.concurrent)) {
+    errors.push(logError('concurrent', 'Should be true or false', config.concurrent))
+  }
+
+  if (!isInteger(config.chunkSize)) {
+    errors.push(logError('chunkSize', 'Should be a number', config.chunkSize))
+  }
+
+  if (isObject(config.globOptions)) {
+    if (!isBoolean(config.globOptions.matchBase)) {
+      errors.push(
+        logError('globOptions.matchBase', 'Should be true or false', config.globOptions.matchBase)
+      )
     }
+
+    if (!isBoolean(config.globOptions.dot)) {
+      errors.push(logError('globOptions.dot', 'Should be true or false', config.globOptions.dot))
+    }
+  } else {
+    errors.push(logError('chunkSize', 'Should be a number', config.chunkSize))
   }
 
-  const deprecatedConfig = {
-    gitDir: () => optRmMsg('gitDir', "lint-staged now automatically resolves '.git' directory."),
-    verbose: () =>
-      optRmMsg('verbose', `Use the command line flag ${chalk.bold('--debug')} instead.`)
+  if (isObject(config.linters)) {
+    Object.keys(config.linters).forEach(key => {
+      if (!isGlob(key)) {
+        errors.push(logError(`linters[${key}]`, 'Key should be a glob pattern', key))
+      }
+
+      if (
+        (!isArray(config.linters[key]) || config.linters[key].some(item => !isString(item))) &&
+        !isString(config.linters[key])
+      ) {
+        errors.push(logError(`linters[${key}]`, 'Should be a string or an array of strings', key))
+      }
+    })
+  } else {
+    errors.push(logError('linters', 'Should be an object', config.linters))
   }
 
-  validate(config, {
-    exampleConfig,
-    deprecatedConfig,
-    unknown: unknownValidationReporter,
-    recursive: false,
-    comment:
-      'Please refer to https://github.com/okonet/lint-staged#configuration for more information...'
+  if (!isArray(config.ignore) || config.ignore.some(item => !isString(item))) {
+    errors.push(logError('ignore', 'Should be an array of strings', config.ignore))
+  }
+
+  if (!isInteger(config.subTaskConcurrency)) {
+    errors.push(logError('subTaskConcurrency', 'Should be a number', config.subTaskConcurrency))
+  }
+
+  if (
+    config.renderer !== 'update' &&
+    config.renderer !== 'verbose' &&
+    !isFunction(config.renderer)
+  ) {
+    errors.push(
+      logError('renderer', "Should be 'update', 'verbose' or a function.", config.renderer)
+    )
+  }
+
+  if (!isBoolean(config.relative)) {
+    errors.push(logError('relative', 'Should be true or false', config.relative))
+  }
+
+  Object.keys(config)
+    .filter(key => !defaultConfig.hasOwnProperty(key))
+    .forEach(option => {
+      if (option === 'gitDir') {
+        warnings.push(
+          logDeprecation('gitDir', "lint-staged now automatically resolves '.git' directory.")
+        )
+        return
+      }
+
+      if (option === 'verbose') {
+        warnings.push(
+          logDeprecation('verbose', `Use the command line flag ${chalk.bold('--debug')} instead.`)
+        )
+        return
+      }
+
+      warnings.push(unknownValidationReporter(config, option))
+    })
+
+  warnings.forEach(message => {
+    console.warn(message)
   })
+
+  if (errors.length) {
+    throw new Error(errors.join('\n'))
+  }
 
   return config
 }
