f5bed7bad940518fb7161e26fbc9a87b4f091edf
Julien Fontanet <julien.fontanet@isonoe.net>
2019-02-02 19:21:44 +0100

    fix: Display package name when node-please-upgrade is being used (#575)
    Before: `undefined requires at least version 8.6.0 of Node, please upgrade`
    After: `lint-staged requires at least version 8.6.0 of Node, please upgrade`

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 'use strict'
 
 const pkg = require('./package.json')
-require('please-upgrade-node')({
-  engines: {
-    node: '>=8.6.0'
-  }
-})
+require('please-upgrade-node')(
+  Object.assign({}, pkg, {
+    engines: {
+      node: '>=8.6.0'
+    }
+  })
+)
 
 const cmdline = require('commander')
 const debugLib = require('debug')
