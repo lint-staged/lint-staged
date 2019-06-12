813b495b2ccdafd34c4d0db9029de266b263bace
Hendrik Schalekamp <carnun@sudo.org.za>
2019-06-12 14:47:29 +0200

    Updated resolver to ignore currently set GIT_DIR

--- src/resolveGitDir.js
+++ src/resolveGitDir.js
@@ -5,7 +5,12 @@ const path = require('path')
 
 module.exports = async function resolveGitDir(options) {
   try {
-    const gitDir = await execGit(['rev-parse', '--show-toplevel'], options)
+    // git cli uses GIT_DIR to fast track its response however it might be set to a different path
+    // depending on where the caller initiated this from, hence clear GIT_DIR
+    const gitDir = await execGit(['rev-parse', '--show-toplevel'], {
+      ...options,
+      env: { GIT_DIR: undefined }
+    })
     return path.normalize(gitDir)
   } catch (error) {
     return null
