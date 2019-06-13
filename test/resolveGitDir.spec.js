5892455fc48e38e67eef6a9ecc44b1114302fed3
Hendrik Schalekamp <carnun@sudo.org.za>
2019-06-13 20:45:34 +0200

    fix: Override env GIT_DIR variable to resolve to the correct git dir path (#629)
    Fixes #627

--- test/resolveGitDir.spec.js
+++ test/resolveGitDir.spec.js
@@ -21,6 +21,15 @@ describe('resolveGitDir', () => {
     process.cwd = processCwdBkp
   })
 
+  it('should resolve to the parent dir when .git is in the parent dir even when the GIT_DIR environment variable is set', async () => {
+    const expected = path.dirname(__dirname)
+    const processCwdBkp = process.cwd
+    process.cwd = () => __dirname
+    process.env.GIT_DIR = 'wrong/path/.git' // refer to https://github.com/DonJayamanne/gitHistoryVSCode/issues/233#issuecomment-375769718
+    expect(path.resolve(await resolveGitDir())).toEqual(expected)
+    process.cwd = processCwdBkp
+  })
+
   it('should return null when not in a git directory', async () => {
     const gitDir = await resolveGitDir({ cwd: '/' }) // assume root is not a git directory
     expect(gitDir).toEqual(null)
