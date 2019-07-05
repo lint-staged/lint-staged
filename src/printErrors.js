0dee25b20b3bcfd650f2cf09ac8ee4c4066cba5c
Cameron Hunter <hello@cameronhunter.co.uk>
2019-07-05 15:10:28 -0700

    test: improve test coverage of printErrors

--- src/printErrors.js
+++ src/printErrors.js
@@ -4,7 +4,7 @@
 // Work-around for duplicated error logs, see #142
 const errMsg = err => (err.privateMsg != null ? err.privateMsg : err.message)
 
-module.exports = function printErrors(errorInstance, logger = console) {
+module.exports = function printErrors(errorInstance, logger) {
   if (Array.isArray(errorInstance.errors)) {
     errorInstance.errors.forEach(lintError => {
       logger.error(errMsg(lintError))
