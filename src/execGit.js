2ce14a3f523cc84b5d1d07963027cead3ccfcd73
Iiro Jäppinen <iiro@jappinen.fi>
2019-07-28 21:12:47 +0300

    refactor: remove unnecessary try/catch

--- src/execGit.js
+++ src/execGit.js
@@ -5,13 +5,9 @@ const execa = require('execa')
 
 module.exports = async function execGit(cmd, options = {}) {
   debug('Running git command', cmd)
-  try {
-    const { stdout } = await execa('git', [].concat(cmd), {
-      ...options,
-      cwd: options.cwd || process.cwd()
-    })
-    return stdout
-  } catch (err) {
-    throw new Error(err)
-  }
+  const { stdout } = await execa('git', [].concat(cmd), {
+    ...options,
+    cwd: options.cwd || process.cwd()
+  })
+  return stdout
 }
