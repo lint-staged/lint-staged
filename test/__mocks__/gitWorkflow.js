7d0379db4f61a0ef460133e0488d2766777bbf32
Iiro Jäppinen <iiro@jappinen.fi>
2019-10-21 17:14:12 +0300

    feat: automatically stage task modifications

--- test/__mocks__/gitWorkflow.js
+++ test/__mocks__/gitWorkflow.js
@@ -1,6 +1,6 @@
 const stub = {
   stashBackup: jest.fn().mockImplementation(() => Promise.resolve()),
-  restoreUnstagedChanges: jest.fn().mockImplementation(() => Promise.resolve()),
+  applyModifications: jest.fn().mockImplementation(() => Promise.resolve()),
   restoreOriginalState: jest.fn().mockImplementation(() => Promise.resolve()),
   dropBackup: jest.fn().mockImplementation(() => Promise.resolve())
 }
