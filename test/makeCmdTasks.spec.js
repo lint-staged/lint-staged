abe4b92d7f6213b59d756d172298bc29bb2bd44c
Iiro Jäppinen <iiro@jappinen.fi>
2020-02-21 09:35:50 +0200

    fix: evaluate functional configuration only once

--- test/makeCmdTasks.spec.js
+++ test/makeCmdTasks.spec.js
@@ -61,7 +61,7 @@ describe('makeCmdTasks', () => {
   it('should work with function task returning a string', async () => {
     const res = await makeCmdTasks({ commands: () => 'test', gitDir, files: ['test.js'] })
     expect(res.length).toBe(1)
-    expect(res[0].title).toEqual('test')
+    expect(res[0].title).toEqual('[Function] test ...')
   })
 
   it('should work with function task returning array of string', async () => {
@@ -71,8 +71,8 @@ describe('makeCmdTasks', () => {
       files: ['test.js']
     })
     expect(res.length).toBe(2)
-    expect(res[0].title).toEqual('test')
-    expect(res[1].title).toEqual('test2')
+    expect(res[0].title).toEqual('[Function] test ...')
+    expect(res[1].title).toEqual('[Function] test2 ...')
   })
 
   it('should work with function task accepting arguments', async () => {
@@ -82,8 +82,8 @@ describe('makeCmdTasks', () => {
       files: ['test.js', 'test2.js']
     })
     expect(res.length).toBe(2)
-    expect(res[0].title).toEqual('test [file]')
-    expect(res[1].title).toEqual('test [file]')
+    expect(res[0].title).toEqual('[Function] test ...')
+    expect(res[1].title).toEqual('[Function] test ...')
   })
 
   it('should work with array of mixed string and function tasks', async () => {
@@ -93,26 +93,31 @@ describe('makeCmdTasks', () => {
       files: ['test.js', 'test2.js', 'test3.js']
     })
     expect(res.length).toBe(5)
-    expect(res[0].title).toEqual('test')
+    expect(res[0].title).toEqual('[Function] test ...')
     expect(res[1].title).toEqual('test2')
-    expect(res[2].title).toEqual('test [file]')
-    expect(res[3].title).toEqual('test [file]')
-    expect(res[4].title).toEqual('test [file]')
-  })
-
-  it('should generate short names for function tasks with long file list', async () => {
-    const res = await makeCmdTasks({
-      commands: filenames => `test ${filenames.map(file => `--file ${file}`).join(' ')}`,
-      gitDir,
-      files: Array(100).fill('file.js') // 100 times `file.js`
-    })
-    expect(res.length).toBe(1)
-    expect(res[0].title).toEqual('test --file [file]')
+    expect(res[2].title).toEqual('[Function] test ...')
+    expect(res[3].title).toEqual('[Function] test ...')
+    expect(res[4].title).toEqual('[Function] test ...')
   })
 
   it('should work with async function tasks', async () => {
     const res = await makeCmdTasks({ commands: async () => 'test', gitDir, files: ['test.js'] })
     expect(res.length).toBe(1)
-    expect(res[0].title).toEqual('test')
+    expect(res[0].title).toEqual('[Function] test ...')
+  })
+
+  it("should throw when function task doesn't return string | string[]", async () => {
+    await expect(makeCmdTasks({ commands: () => null, gitDir, files: ['test.js'] })).rejects
+      .toThrowErrorMatchingInlineSnapshot(`
+"● Validation Error:
+
+  Invalid value for '[Function]'.
+
+  Function task should return a string or an array of strings.
+ 
+  Configured value is: null
+
+Please refer to https://github.com/okonet/lint-staged#configuration for more information..."
+`)
   })
 })
