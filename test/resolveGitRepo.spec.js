5e8bb4c8d18713409dbdd6e0a08910a19b7c3925
Alexander Rinass <alex@fournova.com>
2020-06-12 09:35:11 +0200

    fix: Git directory is not correctly resolved if GIT_WORK_TREE is set to relative path
    Resolving the git directory failed if the environment variable `GIT_WORK_TREE` is set to a relative path and lint-staged is run from a subdirectory of a Git repository (e.g. multi-package repos).
    
    `GIT_WORK_TREE` can be used to set the worktree directory for a Git repo. `GIT_DIR` specifies only the `.git` directory path. `git rev-parse --show-toplevel` will return the Git worktree directory. If it is set to a relative path, it won't work when git commands are called from a subdirectory, as the relative path will fail to resolve correctly.
    
    While `GIT_DIR` is unset in `resolveGitRepo.js`, `GIT_WORK_TREE` was not. Unsetting `GIT_WORK_TREE` in the same place fixes the issue.

--- test/resolveGitRepo.spec.js
+++ test/resolveGitRepo.spec.js
@@ -34,6 +34,16 @@ describe('resolveGitRepo', () => {
     process.cwd = processCwdBkp
   })
 
+  it('should resolve to the parent dir when .git is in the parent dir even when the GIT_WORK_TREE environment variable is set', async () => {
+    const expected = normalize(path.dirname(__dirname))
+    const processCwdBkp = process.cwd
+    process.cwd = () => __dirname
+    process.env.GIT_WORK_TREE = './wrong/path/'
+    const { gitDir } = await resolveGitRepo()
+    expect(gitDir).toEqual(expected)
+    process.cwd = processCwdBkp
+  })
+
   it('should return null when not in a git directory', async () => {
     const { gitDir } = await resolveGitRepo({ cwd: '/' }) // assume root is not a git directory
     expect(gitDir).toEqual(null)
