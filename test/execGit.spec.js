86c9ed2bde1245a702635b42a57b92b809340b0e
Iiro Jäppinen <iiro@jappinen.fi>
2020-06-17 13:02:01 +0300

    fix: run all git commands with submodule.recurse=false (#888)
    This explicitly prevents git commands from recursing into git submodules and thus causing issues with commands like stash or reset.

--- test/execGit.spec.js
+++ test/execGit.spec.js
@@ -1,17 +1,33 @@
 import path from 'path'
 import execa from 'execa'
-import execGit from '../lib/execGit'
+
+import execGit, { GIT_GLOBAL_OPTIONS } from '../lib/execGit'
+
+test('GIT_GLOBAL_OPTIONS', () => {
+  expect(GIT_GLOBAL_OPTIONS).toMatchInlineSnapshot(`
+    Array [
+      "-c",
+      "submodule.recurse=false",
+    ]
+  `)
+})
 
 describe('execGit', () => {
   it('should execute git in process.cwd if working copy is not specified', async () => {
     const cwd = process.cwd()
     await execGit(['init', 'param'])
-    expect(execa).toHaveBeenCalledWith('git', ['init', 'param'], { all: true, cwd })
+    expect(execa).toHaveBeenCalledWith('git', [...GIT_GLOBAL_OPTIONS, 'init', 'param'], {
+      all: true,
+      cwd,
+    })
   })
 
   it('should execute git in a given working copy', async () => {
     const cwd = path.join(process.cwd(), 'test', '__fixtures__')
     await execGit(['init', 'param'], { cwd })
-    expect(execa).toHaveBeenCalledWith('git', ['init', 'param'], { all: true, cwd })
+    expect(execa).toHaveBeenCalledWith('git', [...GIT_GLOBAL_OPTIONS, 'init', 'param'], {
+      all: true,
+      cwd,
+    })
   })
 })
