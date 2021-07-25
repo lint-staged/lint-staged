4ae814344f3d2cbc4688372ea4038aa977998ca9
Iiro Jäppinen <iiro@jappinen.fi>
2021-07-25 19:25:08 +0300

    test: add some more tests

--- test/validateBraces.spec.js
+++ test/validateBraces.spec.js
@@ -84,6 +84,35 @@ describe('validateBraces', () => {
     `)
   })
 
+  /**
+   * @todo This isn't correctly detected even though the outer braces are invalid.
+   */
+  it.skip('should warn about `*.{{js,ts}}` and return fixed pattern', () => {
+    const logger = makeConsoleMock()
+
+    const fixedBraces = validateBraces('*.{{js,ts}}', logger)
+
+    expect(fixedBraces).toEqual('*.{js,ts}')
+    expect(logger.printHistory()).toMatchInlineSnapshot(`
+      "
+      WARN ‼ Detected incorrect braces with only single value: \`*.{{js,ts}}\`. Reformatted as: \`*.{js,ts}\`
+      "
+    `)
+  })
+
+  it('should warn about `*.{{js,ts},{css}}` and return fixed pattern', () => {
+    const logger = makeConsoleMock()
+
+    const fixedBraces = validateBraces('*.{{js,ts},{css}}', logger)
+
+    expect(fixedBraces).toEqual('*.{{js,ts},css}')
+    expect(logger.printHistory()).toMatchInlineSnapshot(`
+      "
+      WARN ‼ Detected incorrect braces with only single value: \`*.{{js,ts},{css}}\`. Reformatted as: \`*.{{js,ts},css}\`
+      "
+    `)
+  })
+
   it('should not warn about `*.\\{js\\}` and return the same pattern', () => {
     const logger = makeConsoleMock()
 
