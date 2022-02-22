2408d0688d15f0f42bfdff401910c4fb1f3ed9f1
Iiro Jäppinen <iiro@jappinen.fi>
2022-02-22 21:09:29 +0200

    refactor: extract `parseGitZOutput` util

---
+++ test/parseGitZOutput.spec.js
@@ -0,0 +1,13 @@
+import { parseGitZOutput } from '../lib/parseGitZOutput'
+
+describe('parseGitZOutput', () => {
+  it('should split string from `git -z` control character', () => {
+    const input = 'a\u0000b\u0000c'
+    expect(parseGitZOutput(input)).toEqual(['a', 'b', 'c'])
+  })
+
+  it('should remove trailing `git -z` control character', () => {
+    const input = 'a\u0000'
+    expect(parseGitZOutput(input)).toEqual(['a'])
+  })
+})
