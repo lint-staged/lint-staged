b0e8aeaa42311b93797f93d8afc7a57792e0c7d1
Tomer Aberbach <tomeraberbach@gmail.com>
2022-03-15 19:55:06 -0400

    fix: force update rendering for forced colors and further ensure quiet rendering when requested

--- test/getRenderer.spec.js
+++ test/getRenderer.spec.js
@@ -1,28 +1,61 @@
 import { getRenderer } from '../lib/getRenderer'
 
 describe('getRenderer', () => {
-  it('should return silent renderer when quiet', () => {
-    expect(getRenderer({ quiet: true }, {})).toEqual({ renderer: 'silent' })
+  it('should return silent renderers when quiet', () => {
+    expect(getRenderer({ quiet: true }, {})).toEqual({
+      renderer: 'silent',
+      nonTTYRenderer: 'silent',
+    })
   })
 
-  it('should return verbose renderer when NODE_ENV=test', () => {
-    expect(getRenderer({}, { NODE_ENV: 'test' })).toEqual({ renderer: 'verbose' })
+  it('should return verbose renderers when NODE_ENV=test', () => {
+    expect(getRenderer({}, { NODE_ENV: 'test' })).toEqual({
+      renderer: 'verbose',
+      nonTTYRenderer: 'verbose',
+    })
   })
 
-  it('should return test renderer when TERM=dumb', () => {
-    expect(getRenderer({}, { TERM: 'dumb' })).toEqual({ renderer: 'verbose' })
+  it('should return test renderers when TERM=dumb', () => {
+    expect(getRenderer({}, { TERM: 'dumb' })).toEqual({
+      renderer: 'verbose',
+      nonTTYRenderer: 'verbose',
+    })
   })
 
-  it('should return verbose renderer when debug', () => {
-    expect(getRenderer({ debug: true }, {})).toEqual({ renderer: 'verbose' })
+  it('should return verbose renderers when debug', () => {
+    expect(getRenderer({ debug: true }, {})).toEqual({
+      renderer: 'verbose',
+      nonTTYRenderer: 'verbose',
+    })
   })
 
-  it('should return update renderer by default', () => {
+  it('should return update main renderer and verbose fallback renderer by default', () => {
     expect(getRenderer({}, {})).toEqual({
       renderer: 'update',
       rendererOptions: {
         dateFormat: false,
       },
+      nonTTYRenderer: 'verbose',
+    })
+  })
+
+  it('should return update main renderer and verbose fallback renderer when colors are not forced', () => {
+    expect(getRenderer({}, { FORCE_COLOR: '0' })).toEqual({
+      renderer: 'update',
+      rendererOptions: {
+        dateFormat: false,
+      },
+      nonTTYRenderer: 'verbose',
+    })
+  })
+
+  it('should return update renderers when colors are forced', () => {
+    expect(getRenderer({}, { FORCE_COLOR: '1' })).toEqual({
+      renderer: 'update',
+      rendererOptions: {
+        dateFormat: false,
+      },
+      nonTTYRenderer: 'update',
     })
   })
 })
