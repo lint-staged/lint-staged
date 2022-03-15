f13dbb7004b3f470de69053bf4edb74d3dfb3e6f
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2022-03-15 21:02:57 +0100

    chore: don't use touch on windows

--- test/utils/crossPlatform.js
+++ test/utils/crossPlatform.js
@@ -1,3 +1,7 @@
+import os from 'os'
+
+export const isWindows = os.platform() === 'win32'
+
 /** Whether the current environment is a GitHub Actions runner under Windows */
 export const isWindowsActions = () => {
   const { GITHUB_ACTIONS, RUNNER_OS } = process.env
