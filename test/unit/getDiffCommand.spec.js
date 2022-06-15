8c266fb185136bea1970a0dc598ccf2a6bc42140
Andrii Miroshnychenko <andrew.miroshnichenko@gmail.com>
2022-06-15 15:39:52 +0200

    fix: <diff and diffFilter params are now used also to determine changes after tasks were applied>

---
+++ test/unit/getDiffCommand.spec.js
@@ -0,0 +1,51 @@
+import { getDiffCommand } from '../../lib/getDiffCommand.js'
+
+describe('chunkFiles', () => {
+  const customDiffString = 'origin/main..custom-branch'
+  const customDiffSpaceSeparatedString = 'origin/main custom-branch'
+  const customDiffFilter = 'a'
+
+  it('should default to sane value', () => {
+    const diff = getDiffCommand()
+    expect(diff).toEqual(['diff', '--name-only', '-z', `--diff-filter=ACMR`, '--staged'])
+  })
+
+  it('should work only with diff set as string', () => {
+    const diff = getDiffCommand(customDiffString)
+    expect(diff).toEqual([
+      'diff',
+      '--name-only',
+      '-z',
+      `--diff-filter=ACMR`,
+      'origin/main..custom-branch',
+    ])
+  })
+
+  it('should work only with diff set as space separated string', () => {
+    const diff = getDiffCommand(customDiffSpaceSeparatedString)
+    expect(diff).toEqual([
+      'diff',
+      '--name-only',
+      '-z',
+      `--diff-filter=ACMR`,
+      'origin/main',
+      'custom-branch',
+    ])
+  })
+
+  it('should work only with diffFilter set', () => {
+    const diff = getDiffCommand(undefined, customDiffFilter)
+    expect(diff).toEqual(['diff', '--name-only', '-z', `--diff-filter=a`, '--staged'])
+  })
+
+  it('should work with both diff and diffFilter set', () => {
+    const diff = getDiffCommand(customDiffString, customDiffFilter)
+    expect(diff).toEqual([
+      'diff',
+      '--name-only',
+      '-z',
+      `--diff-filter=a`,
+      'origin/main..custom-branch',
+    ])
+  })
+})
