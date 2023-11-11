ab33218842c81586296ffc1c2e5efb62c46a3f8e
Iiro Jäppinen <iiro@jappinen.fi>
2023-11-11 10:30:38 +0200

    test: add integration test for symbolic link git dir

---
+++ test/integration/symlink-git-dir.test.js
@@ -0,0 +1,38 @@
+import './__mocks__/resolveConfig.js'
+
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+import { jest } from '@jest/globals'
+
+import { withGitIntegration } from './__utils__/withGitIntegration.js'
+import { prettierListDifferent } from './__fixtures__/configs.js'
+import { prettyJS } from './__fixtures__/files.js'
+
+jest.setTimeout(20000)
+jest.retryTimes(2)
+
+describe('lint-staged', () => {
+  test(
+    'supports symlinked git dir',
+    withGitIntegration(async ({ cwd, execGit, gitCommit, readFile, writeFile }) => {
+      // Rename `.git` to `git` and add symbolic link pointing to it
+      await fs.rename(path.resolve(cwd, '.git'), path.resolve(cwd, 'git'))
+      await fs.symlink(path.resolve(cwd, 'git'), path.resolve(cwd, '.git'), 'dir')
+
+      await writeFile('.lintstagedrc.json', JSON.stringify(prettierListDifferent))
+
+      // Stage pretty file
+      await writeFile('test file.js', prettyJS)
+      await execGit(['add', 'test file.js'])
+
+      // Run lint-staged with `prettier --list-different` and commit pretty file
+      await gitCommit()
+
+      // Nothing is wrong, so a new commit is created
+      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('2')
+      expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('test')
+      expect(await readFile('test file.js')).toEqual(prettyJS)
+    })
+  )
+})
