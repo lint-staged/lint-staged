659387011754eeed818646ce594a772f3106e49a
Iiro Jäppinen <iiro@jappinen.fi>
2024-06-10 19:13:41 +0300

    ci: test Node.js versions against current instead of latest dependency

--- scripts/test-node-range.js
+++ scripts/test-node-range.js
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises'
 import { fileURLToPath } from 'node:url'
 
 import chalk from 'chalk'
-import { execa } from 'execa'
+import { execaCommand } from 'execa'
 import { subset } from 'semver'
 
 const packageJson = JSON.parse(
@@ -19,11 +19,20 @@ console.log(chalk.dim('-----------------------------'))
 
 let allDependenciesSupported = true
 
-for (const dependency of Object.keys(packageJson.dependencies)) {
-  const { stdout: dependencyRequires } = await execa('npm', ['info', dependency, 'engines.node'])
+for (const [dependency, version] of Object.entries(packageJson.dependencies)) {
+  /**
+   * @example <caption>when matching single version</caption>
+   * "">=18"
+   * @example <caption>when matching multiple versions</caption>
+   * [">=6.0", ">=6.0"]
+   */
+  const { stdout } = await execaCommand(`npm info ${dependency}@${version} engines.node --json`)
+  const json = JSON.parse(stdout)
+
+  const requiredVersion = Array.isArray(json) ? json[json.length - 1] : json
 
   /** True if currently-required range is inside the dependency's required range */
-  const isSubset = subset(lintStagedRequires, dependencyRequires)
+  const isSubset = subset(lintStagedRequires, requiredVersion)
 
   if (!isSubset) {
     allDependenciesSupported = false
@@ -32,7 +41,7 @@ for (const dependency of Object.keys(packageJson.dependencies)) {
   const color = isSubset ? chalk.greenBright : chalk.redBright
   const symbol = isSubset ? `✓` : '×'
 
-  console.log(`${color(`${symbol} ${dependency}`)}:`, dependencyRequires)
+  console.log(`${color(`${symbol} ${dependency}`)}:`, requiredVersion)
 }
 
 if (!allDependenciesSupported) {
