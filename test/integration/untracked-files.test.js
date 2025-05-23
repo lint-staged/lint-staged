2515f888f5e032fcd0ccf220986945452a3210c9
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-23 03:03:38 -0600

    commit: merge untracked-changes-test into untracked-files-test

--- test/integration/untracked-files.test.js
+++ test/integration/untracked-files.test.js
@@ -1,7 +1,9 @@
 import { jest } from '@jest/globals'
 
 import { prettierListDifferent } from './__fixtures__/configs.js'
+import * as configFixtures from './__fixtures__/configs.js'
 import * as fileFixtures from './__fixtures__/files.js'
+import { normalizeWindowsNewlines } from './__utils__/normalizeWindowsNewlines.js'
 import { withGitIntegration } from './__utils__/withGitIntegration.js'
 
 jest.setTimeout(20000)
@@ -57,6 +59,65 @@ describe('lint-staged', () => {
       expect(await readFile('test.js')).toEqual(fileFixtures.invalidJS)
       expect(await readFile('test-untracked.js')).toEqual(fileFixtures.prettyJS)
       expect(Buffer.from(await readFile('binary'), 'binary').toString()).toEqual('Hello, World!')
+    })
+  )
+
+  test(
+    'restores untracked files with --hide-untracked option when no errors from linter',
+    withGitIntegration(async ({ appendFile, execGit, gitCommit, readFile }) => {
+      await appendFile('.lintstagedrc.json', JSON.stringify(configFixtures.prettierListDifferent))
+
+      // Stage pretty file
+      await appendFile('test.js', fileFixtures.prettyJS)
+      await execGit(['add', 'test.js'])
+
+      // Create untracked file
+      await appendFile('test2.js', fileFixtures.prettyJS)
+
+      const output = await gitCommit({ lintStaged: { hideUntracked: true } })
+
+      expect(output).toMatch('Hiding unstaged changes...')
+      expect(output).toMatch('Applying modifications from tasks...')
+      expect(output).toMatch('Restoring unstaged changes...')
+
+      // Nothing is wrong, so a new commit is created and file is pretty
+      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('2')
+      expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('test')
+
+      // Since file is untracked, it is still present
+      const status = await execGit(['status'])
+      expect(status).toMatch('test2.js')
+      expect(status).toMatch('nothing added to commit but untracked files present')
+
+      /** @todo `git` in GitHub Windows runners seem to add `\r\n` newlines in this case. */
+      expect(normalizeWindowsNewlines(await readFile('test.js'))).toEqual(fileFixtures.prettyJS)
+    })
+  )
+
+  test(
+    'restores untracked files with --hide-untracked option when errors from linter',
+    withGitIntegration(async ({ appendFile, execGit, gitCommit, readFile }) => {
+      await appendFile('.lintstagedrc.json', JSON.stringify(configFixtures.prettierListDifferent))
+
+      // Stage ugly file
+      await appendFile('test.js', fileFixtures.uglyJS)
+      await execGit(['add', 'test.js'])
+
+      // Create untracked file
+      await appendFile('test2.js', fileFixtures.prettyJS)
+
+      const status = await execGit(['status'])
+
+      // Run lint-staged with `prettier --list-different` to break the linter
+      await expect(gitCommit({ lintStaged: { hideUntracked: true } })).rejects.toThrow(
+        'Reverting to original state because of errors'
+      )
+
+      // Something was wrong so the repo is returned to original state
+      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('1')
+      expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('initial commit')
+      expect(await execGit(['status'])).toEqual(status)
+      expect(await readFile('test2.js')).toEqual(fileFixtures.prettyJS)
     })
   )
 })
