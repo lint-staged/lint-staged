7fa7caf4a3be4b27b2b8f6fc940f07f068512725
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-23 01:25:38 -0600

    commit: merge hideUntrackedFiles into hideUnstagedChanges

--- test/integration/untracked-changes.test.js
+++ test/integration/untracked-changes.test.js
@@ -17,14 +17,8 @@ describe('lint-staged', () => {
     withGitIntegration(async ({ appendFile, execGit, gitCommit, readFile }) => {
       await appendFile('.lintstagedrc.json', JSON.stringify(configFixtures.prettierListDifferent))
 
-      // Stage and commit pretty files
-      await appendFile('test.js', fileFixtures.prettyJS)
-      await execGit(['add', 'test.js'])
-      await execGit(['commit', '-m', 'add test file'])
-
       // Stage pretty file
-      const appended = `\nconsole.log("test");\n`
-      await appendFile('test.js', appended)
+      await appendFile('test.js', fileFixtures.prettyJS)
       await execGit(['add', 'test.js'])
 
       // Create untracked file
@@ -32,12 +26,12 @@ describe('lint-staged', () => {
 
       const output = await gitCommit({ lintStaged: { hideUntracked: true } })
 
-      expect(output).toMatch('Hiding untracked files...')
+      expect(output).toMatch('Hiding unstaged changes...')
       expect(output).toMatch('Applying modifications from tasks...')
       expect(output).toMatch('Restoring unstaged changes...')
 
       // Nothing is wrong, so a new commit is created and file is pretty
-      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('3')
+      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('2')
       expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('test')
 
       // Since file is untracked, it is still present
@@ -46,9 +40,7 @@ describe('lint-staged', () => {
       expect(status).toMatch('nothing added to commit but untracked files present')
 
       /** @todo `git` in GitHub Windows runners seem to add `\r\n` newlines in this case. */
-      expect(normalizeWindowsNewlines(await readFile('test.js'))).toEqual(
-        fileFixtures.prettyJS + appended
-      )
+      expect(normalizeWindowsNewlines(await readFile('test.js'))).toEqual(fileFixtures.prettyJS)
     })
   )
 
@@ -57,12 +49,7 @@ describe('lint-staged', () => {
     withGitIntegration(async ({ appendFile, execGit, gitCommit, readFile }) => {
       await appendFile('.lintstagedrc.json', JSON.stringify(configFixtures.prettierListDifferent))
 
-      // Stage and commit pretty files
-      await appendFile('test.js', fileFixtures.prettyJS)
-      await execGit(['add', 'test.js'])
-      await execGit(['commit', '-m', 'add test file'])
-
-      // Stage pretty file
+      // Stage ugly file
       await appendFile('test.js', fileFixtures.uglyJS)
       await execGit(['add', 'test.js'])
 
@@ -77,8 +64,8 @@ describe('lint-staged', () => {
       )
 
       // Something was wrong so the repo is returned to original state
-      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('2')
-      expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('add test file')
+      expect(await execGit(['rev-list', '--count', 'HEAD'])).toEqual('1')
+      expect(await execGit(['log', '-1', '--pretty=%B'])).toMatch('initial commit')
       expect(await execGit(['status'])).toEqual(status)
       expect(await readFile('test2.js')).toEqual(fileFixtures.prettyJS)
     })
