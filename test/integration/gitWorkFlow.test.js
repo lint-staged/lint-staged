8db2717574ebfa2b80e0fc4eb0b24d705fd264fc
Iiro Jäppinen <iiro@jappinen.fi>
2025-09-18 20:44:58 +0300

    feat: add new option "--hide-unstaged"

--- test/integration/gitWorkFlow.test.js
+++ test/integration/gitWorkFlow.test.js
@@ -35,7 +35,7 @@ describe('gitWorkflow', () => {
         jest.doMock('execa', () => Promise.reject({}))
         const ctx = getInitialState()
         // mock a simple failure
-        gitWorkflow.getPartiallyStagedFiles = () => ['foo']
+        gitWorkflow.getUnstagedFiles = () => ['foo']
         gitWorkflow.getHiddenFilepath = () => {
           throw new Error('test')
         }
@@ -70,7 +70,23 @@ describe('gitWorkflow', () => {
     )
   })
 
-  describe('getPartiallyStagedFiles', () => {
+  describe('getUnstagedFiles', () => {
+    it(
+      'should return null when no unstaged changes',
+      withGitIntegration(async ({ appendFile, cwd, execGit }) => {
+        const gitWorkflow = new GitWorkflow({
+          topLevelDir: cwd,
+          gitConfigDir: path.join(cwd, './.git'),
+        })
+        await appendFile('file with spaces.txt', 'staged content')
+        await appendFile('file_without_spaces.txt', 'staged content')
+        await execGit(['add', 'file with spaces.txt'])
+        await execGit(['add', 'file_without_spaces.txt'])
+
+        expect(await gitWorkflow.getUnstagedFiles({ onlyPartial: false })).toStrictEqual(null)
+      })
+    )
+
     it(
       'should return unquoted files',
       withGitIntegration(async ({ appendFile, cwd, execGit }) => {
@@ -85,9 +101,28 @@ describe('gitWorkflow', () => {
         await appendFile('file with spaces.txt', 'not staged content')
         await appendFile('file_without_spaces.txt', 'not staged content')
 
-        expect(await gitWorkflow.getPartiallyStagedFiles()).toStrictEqual([
+        expect(await gitWorkflow.getUnstagedFiles({ onlyPartial: false })).toStrictEqual([
           'file with spaces.txt',
           'file_without_spaces.txt',
+        ])
+      })
+    )
+
+    it(
+      'should return only partially changes files',
+      withGitIntegration(async ({ appendFile, cwd, execGit }) => {
+        const gitWorkflow = new GitWorkflow({
+          topLevelDir: cwd,
+          gitConfigDir: path.join(cwd, './.git'),
+        })
+        await appendFile('file with spaces.txt', 'staged content')
+        await appendFile('file_without_spaces.txt', 'staged content')
+        await execGit(['add', 'file with spaces.txt'])
+        await appendFile('file with spaces.txt', 'not staged content')
+        await appendFile('file_without_spaces.txt', 'not staged content')
+
+        expect(await gitWorkflow.getUnstagedFiles({ onlyPartial: true })).toStrictEqual([
+          'file with spaces.txt',
         ])
       })
     )
@@ -106,7 +141,7 @@ describe('gitWorkflow', () => {
         await appendFile('original.txt', 'additional content')
         await execGit(['mv', 'original.txt', 'renamed.txt'])
 
-        expect(await gitWorkflow.getPartiallyStagedFiles()).toStrictEqual([
+        expect(await gitWorkflow.getUnstagedFiles()).toStrictEqual([
           'renamed.txt\u0000original.txt',
         ])
       })
@@ -123,7 +158,7 @@ describe('gitWorkflow', () => {
         })
 
         const totallyRandom = `totally_random_file-${Date.now().toString()}`
-        gitWorkflow.partiallyStagedFiles = [totallyRandom]
+        gitWorkflow.unstagedFiles = [totallyRandom]
         const ctx = getInitialState()
         await expect(gitWorkflow.hideUnstagedChanges(ctx)).rejects.toThrow(
           `pathspec '${totallyRandom}' did not match any file(s) known to git`
@@ -147,7 +182,7 @@ describe('gitWorkflow', () => {
         await execGit(['mv', 'README.md', 'TEST.md'])
         await appendFile('TEST.md', 'added content')
 
-        gitWorkflow.partiallyStagedFiles = await gitWorkflow.getPartiallyStagedFiles()
+        gitWorkflow.unstagedFiles = await gitWorkflow.getUnstagedFiles(true)
         const ctx = getInitialState()
         await gitWorkflow.hideUnstagedChanges(ctx)
 
