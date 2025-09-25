6a96b8639f7f1d37dece5618645dc9fa970fe221
Iiro Jäppinen <iiro@jappinen.fi>
2025-09-25 07:01:21 +0300

    refactor: improve logging when config not found in package.json

--- test/unit/dynamicImport.spec.js
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'vitest'
-
-import { dynamicImport } from '../../lib/dynamicImport.js'
-
-describe('dynamicImport', () => {
-  it('should log errors into console', async ({ expect }) => {
-    await expect(() => dynamicImport('not-found.js')).rejects.toThrow(`Cannot find module`)
-  })
-})
