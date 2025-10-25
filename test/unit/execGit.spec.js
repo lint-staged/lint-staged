6eec4582225ff14bac665d083e136e2b97944a8d
Iiro Jäppinen <iiro@jappinen.fi>
2025-10-25 19:47:28 +0300

    feat: use `tinyexec` for Git commands

--- test/unit/execGit.spec.js
+++ test/unit/execGit.spec.js
@@ -1,12 +1,13 @@
 import path from 'node:path'
 
-import { beforeEach, describe, it, test } from 'vitest'
+import { beforeEach, describe, it, test, vi } from 'vitest'
 
-import { getMockNanoSpawn } from './__utils__/getMockNanoSpawn.js'
+import { exec } from '../../lib/exec.js'
+import { execGit, GIT_GLOBAL_OPTIONS } from '../../lib/execGit.js'
 
-const { default: spawn } = await getMockNanoSpawn()
-
-const { execGit, GIT_GLOBAL_OPTIONS } = await import('../../lib/execGit.js')
+vi.mock('../../lib/exec.js', () => ({
+  exec: vi.fn().mockReturnValue({ result: vi.fn() }),
+}))
 
 test('GIT_GLOBAL_OPTIONS', ({ expect }) => {
   expect(GIT_GLOBAL_OPTIONS).toEqual(['-c', 'submodule.recurse=false'])
@@ -14,24 +15,14 @@ test('GIT_GLOBAL_OPTIONS', ({ expect }) => {
 
 describe('execGit', () => {
   beforeEach(() => {
-    spawn.mockReset()
-  })
-
-  it('should execute git in process.cwd if working copy is not specified', async ({ expect }) => {
-    const cwd = process.cwd()
-    await execGit(['init', 'param'])
-    expect(spawn).toHaveBeenCalledExactlyOnceWith('git', [...GIT_GLOBAL_OPTIONS, 'init', 'param'], {
-      cwd,
-      stdin: 'ignore',
-    })
+    vi.clearAllMocks()
   })
 
   it('should execute git in a given working copy', async ({ expect }) => {
     const cwd = path.join(process.cwd(), 'test', '__fixtures__')
     await execGit(['init', 'param'], { cwd })
-    expect(spawn).toHaveBeenCalledExactlyOnceWith('git', [...GIT_GLOBAL_OPTIONS, 'init', 'param'], {
+    expect(exec).toHaveBeenCalledExactlyOnceWith('git', [...GIT_GLOBAL_OPTIONS, 'init', 'param'], {
       cwd,
-      stdin: 'ignore',
     })
   })
 })
