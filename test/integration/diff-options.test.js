f217cfcc46090de4d0db91f9ba17fd7e9f290dac
Iiro Jäppinen <iiro@jappinen.fi>
2026-02-28 11:41:37 +0200

    test: adjust integration test logging setup for concurrency

--- test/integration/diff-options.test.js
+++ test/integration/diff-options.test.js
@@ -9,8 +9,7 @@ describe('lint-staged', () => {
   test(
     'supports overriding file list using --diff',
     withGitIntegration(async ({ appendFile, cwd, execGit, expect }) => {
-      const globalConsoleTemp = console
-      console = makeConsoleMock()
+      const console = makeConsoleMock()
 
       // Commit ugly file
       await appendFile('test.js', fileFixtures.uglyJS)
@@ -22,55 +21,54 @@ describe('lint-staged', () => {
 
       // Run lint-staged with `--diff` between the two commits.
       // Nothing is staged at this point, so don't rung `gitCommit`
-      const passed = await lintStaged({
-        config: { '*.js': 'prettier --list-different' },
-        cwd,
-        diff: `${hashes[1]}...${hashes[0]}`,
-        stash: false,
-      })
+      const passed = await lintStaged(
+        {
+          config: { '*.js': 'prettier --list-different' },
+          cwd,
+          diff: `${hashes[1]}...${hashes[0]}`,
+          stash: false,
+        },
+        console
+      )
 
       // Lint-staged failed because commit diff contains ugly file
       expect(passed).toEqual(false)
 
-      expect(console.printHistory()).toMatch('prettier --list-different')
-      expect(console.printHistory()).toMatch('test.js')
-
-      console = globalConsoleTemp
+      expect(console.printHistory()).toMatch('prettier --list-different [FAILED]')
     })
   )
 
   test(
     'supports overriding default --diff-filter',
     withGitIntegration(async ({ appendFile, cwd, execGit, expect }) => {
-      const globalConsoleTemp = console
-      console = makeConsoleMock()
+      const console = makeConsoleMock()
 
       // Stage ugly file
       await appendFile('test.js', fileFixtures.uglyJS)
       await execGit(['add', 'test.js'])
 
       // Run lint-staged with `--diff-filter=D` to include only deleted files.
-      const passed = await lintStaged({
-        config: { '*.js': 'prettier --list-different' },
-        cwd,
-        diffFilter: 'D',
-        stash: false,
-      })
+      const passed = await lintStaged(
+        {
+          config: { '*.js': 'prettier --list-different' },
+          cwd,
+          diffFilter: 'D',
+          stash: false,
+        },
+        console
+      )
 
       // Lint-staged passed because no matching (deleted) files
       expect(passed).toEqual(true)
 
       expect(console.printHistory()).toMatch('could not find any staged files')
-
-      console = globalConsoleTemp
     })
   )
 
   test(
     'supports staged deleted files processed by linter',
     withGitIntegration(async ({ appendFile, cwd, execGit, expect }) => {
-      const globalConsoleTemp = console
-      console = makeConsoleMock()
+      const console = makeConsoleMock()
 
       // Stage and commit ugly file
       await appendFile('test.js', fileFixtures.uglyJS)
@@ -81,24 +79,24 @@ describe('lint-staged', () => {
       await execGit(['rm', 'test.js'])
 
       // Run lint-staged with `--diff-filter=D` to include only deleted files.
-      const passed = await lintStaged({
-        config: { '*.js': 'prettier --list-different --no-error-on-unmatched-pattern' },
-        cwd,
-        diffFilter: 'D',
-      })
+      const passed = await lintStaged(
+        {
+          config: { '*.js': 'prettier --list-different --no-error-on-unmatched-pattern' },
+          cwd,
+          diffFilter: 'D',
+        },
+        console
+      )
 
       expect(passed).toEqual(true)
       expect(console.printHistory()).not.toMatch('No files matching the pattern were found:')
-
-      console = globalConsoleTemp
     })
   )
 
   test(
     'supports staged deleted files restored by linter',
     withGitIntegration(async ({ appendFile, cwd, execGit, expect }) => {
-      const globalConsoleTemp = console
-      console = makeConsoleMock()
+      const console = makeConsoleMock()
 
       // Stage and commit ugly file
       await appendFile('test.js', fileFixtures.uglyJS)
@@ -110,25 +108,25 @@ describe('lint-staged', () => {
 
       // Run lint-staged with `--diff-filter=D` to include only deleted files.
       // Use git to restore deleted staged file and then prettify it
-      const passed = await lintStaged({
-        config: { '*.js': ['git reset --', 'git checkout --', 'prettier --write'] },
-        cwd,
-        diffFilter: 'D',
-      })
+      const passed = await lintStaged(
+        {
+          config: { '*.js': ['git reset --', 'git checkout --', 'prettier --write'] },
+          cwd,
+          diffFilter: 'D',
+        },
+        console
+      )
 
       expect(passed).toEqual(false)
       expect(console.printHistory()).not.toMatch('No files matching the pattern were found:')
       expect(console.printHistory()).toMatch('lint-staged prevented an empty git commit.')
-
-      console = globalConsoleTemp
     })
   )
 
   test(
     'supports staged deleted files not processed by linter',
     withGitIntegration(async ({ appendFile, cwd, execGit, expect }) => {
-      const globalConsoleTemp = console
-      console = makeConsoleMock()
+      const console = makeConsoleMock()
 
       // Stage and commit file
       await appendFile('test.txt', 'text contents')
@@ -139,23 +137,24 @@ describe('lint-staged', () => {
       await execGit(['rm', 'test.txt'])
 
       // Run lint-staged with `--diff-filter=D` to include only deleted files.
-      const passed = await lintStaged({
-        config: {
-          '*.js': 'prettier --list-different',
-          '*': () => [
-            /* despite empty array, this triggers processing of all files */
-          ],
+      const passed = await lintStaged(
+        {
+          config: {
+            '*.js': 'prettier --list-different',
+            '*': () => [
+              /* despite empty array, this triggers processing of all files */
+            ],
+          },
+          cwd,
+          diffFilter: 'D',
         },
-        cwd,
-        diffFilter: 'D',
-      })
+        console
+      )
 
       expect(passed).toEqual(true)
       expect(console.printHistory()).toMatch('Running tasks for staged files...')
       expect(console.printHistory()).not.toMatch('fatal: pathspec')
       expect(console.printHistory()).not.toMatch('did not match any files')
-
-      console = globalConsoleTemp
     })
   )
 })
