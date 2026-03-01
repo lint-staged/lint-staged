4f88ddf27f76d5c56a530b47475dbc079ffa74b6
Iiro Jäppinen <iiro@jappinen.fi>
2026-03-01 16:53:00 +0200

    feat: replace commander.js with Node.js builtins

--- bin/lint-staged.js
+++ bin/lint-staged.js
@@ -2,150 +2,31 @@
 
 import { userInfo } from 'node:os'
 
-import { Option, program } from 'commander'
-
+import { getVersionNumber, parseCliOptions, printHelpText } from '../lib/cli.js'
 import { createDebug, enableDebug } from '../lib/debug.js'
 import lintStaged from '../lib/index.js'
-import { CONFIG_STDIN_ERROR, restoreStashExample } from '../lib/messages.js'
+import { CONFIG_STDIN_ERROR } from '../lib/messages.js'
 import { readStdin } from '../lib/readStdin.js'
-import { getVersion } from '../lib/version.js'
 
 const debugLog = createDebug('lint-staged:bin')
 
 // Do not terminate main Listr process on SIGINT
 process.on('SIGINT', () => {})
 
-program
-  .version(await getVersion())
+const cliOptions = parseCliOptions(process.argv)
 
-  /**
-   * This shouldn't be necessary for lint-staged, but add migration step just in case
-   * to preserve old behavior of "commander".
-   *
-   * @todo remove this in the major version
-   * @see https://github.com/tj/commander.js/releases/tag/v13.0.0
-   * */
-  .allowExcessArguments()
+if (cliOptions.version) {
+  console.log(await getVersionNumber())
+  process.exit(0)
+}
 
-  .addOption(
-    new Option('--allow-empty', 'allow empty commits when tasks revert all staged changes').default(
-      false
-    )
-  )
-  .addOption(
-    new Option(
-      '-p, --concurrent <number|boolean>',
-      'the number of tasks to run concurrently, or false for serial'
-    ).default(true)
-  )
-  .addOption(
-    new Option('-c, --config [path]', 'path to configuration file, or - to read from stdin')
-  )
-  .addOption(
-    new Option('--cwd [path]', 'run all tasks in specific directory, instead of the current')
-  )
-  .addOption(new Option('-d, --debug', 'print additional debug information').default(false))
-  .addOption(
-    new Option(
-      '--diff [string]',
-      'override the default "--staged" flag of "git diff" to get list of files. Implies "--no-stash".'
-    ).implies({ stash: false })
-  )
-  .addOption(
-    new Option(
-      '--diff-filter [string]',
-      'override the default "--diff-filter=ACMR" flag of "git diff" to get list of files'
-    )
-  )
-  .addOption(
-    new Option('--continue-on-error', 'run all tasks to completion even if one fails').default(
-      false
-    )
-  )
-  .addOption(
-    new Option('--fail-on-changes', 'fail with exit code 1 when tasks modify tracked files')
-      .default(false)
-      .implies({ revert: false })
-  )
-  .addOption(
-    new Option(
-      '--max-arg-length [number]',
-      'maximum length of the command-line argument string'
-    ).default(0)
-  )
-
-  /**
-   * We don't want to show the `--revert` flag because it's on by default, and only show the
-   * negatable flag `--no-rever` instead. There seems to be a bug in Commander.js where
-   * configuring only the latter won't actually set the default value.
-   */
-  .addOption(
-    new Option('--revert', 'revert to original state in case of errors').default(true).hideHelp()
-  )
-  .addOption(
-    new Option('--no-revert', 'do not revert to original state in case of errors.').default(false)
-  )
-
-  .addOption(new Option('--stash', 'enable the backup stash').default(true).hideHelp())
-  .addOption(
-    new Option('--no-stash', 'disable the backup stash. Implies "--no-revert".')
-      .default(false)
-      .implies({ revert: false })
-  )
-
-  .addOption(
-    new Option('--hide-partially-staged', 'hide unstaged changes from partially staged files')
-      .default(true)
-      .hideHelp()
-  )
-  .addOption(
-    new Option(
-      '--no-hide-partially-staged',
-      'disable hiding unstaged changes from partially staged files'
-    ).default(false)
-  )
-
-  .addOption(
-    new Option('--hide-unstaged', 'hide all unstaged changes, instead of just partially staged')
-      .default(false)
-      .implies({ hidePartiallyStaged: false })
-  )
-
-  .addOption(new Option('-q, --quiet', 'disable lint-staged’s own console output').default(false))
-  .addOption(new Option('-r, --relative', 'pass relative filepaths to tasks').default(false))
-  .addOption(
-    new Option(
-      '-v, --verbose',
-      'show task output even when tasks succeed; by default only failed output is shown'
-    ).default(false)
-  )
-
-  .addHelpText('afterAll', '\n' + restoreStashExample())
-
-const cliOptions = program.parse(process.argv).opts()
+if (cliOptions.help) {
+  console.log(await printHelpText())
+  process.exit(0)
+}
 
 if (cliOptions.debug) {
   enableDebug()
-}
-
-const options = {
-  allowEmpty: !!cliOptions.allowEmpty,
-  concurrent: JSON.parse(cliOptions.concurrent),
-  configPath: cliOptions.config,
-  continueOnError: !!cliOptions.continueOnError,
-  cwd: cliOptions.cwd,
-  debug: !!cliOptions.debug,
-  diff: cliOptions.diff,
-  diffFilter: cliOptions.diffFilter,
-  failOnChanges: !!cliOptions.failOnChanges,
-  hidePartiallyStaged: !!cliOptions.hidePartiallyStaged, // commander inverts `no-<x>` flags to `!x`
-  hideUnstaged: !!cliOptions.hideUnstaged,
-  maxArgLength: cliOptions.maxArgLength || undefined,
-  quiet: !!cliOptions.quiet,
-  relative: !!cliOptions.relative,
-  revert: !!cliOptions.revert, // commander inverts `no-<x>` flags to `!x`
-  stash: !!cliOptions.stash, // commander inverts `no-<x>` flags to `!x`
-  verbose: !!cliOptions.verbose,
 }
 
 try {
@@ -155,13 +36,13 @@ try {
   debugLog('Could not determine current shell')
 }
 
-debugLog('Options parsed from command-line: %o', options)
+debugLog('Options parsed from command-line: %o', cliOptions)
 
-if (options.configPath === '-') {
-  delete options.configPath
+if (cliOptions.configPath === '-') {
+  delete cliOptions.configPath
   try {
     debugLog('Reading config from stdin')
-    options.config = JSON.parse(await readStdin())
+    cliOptions.config = JSON.parse(await readStdin())
   } catch (error) {
     debugLog(CONFIG_STDIN_ERROR, error)
     console.error(CONFIG_STDIN_ERROR)
@@ -169,7 +50,7 @@ if (options.configPath === '-') {
   }
 }
 
-const passed = await lintStaged(options)
+const passed = await lintStaged(cliOptions)
 if (!passed) {
   process.exitCode = 1
 }
