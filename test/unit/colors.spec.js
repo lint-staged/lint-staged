998a3415b32cd3964b958b631946be4375e5e8a7
Iiro Jäppinen <iiro@jappinen.fi>
2026-03-26 20:19:35 +0200

    fix: detect colors so that Bun supports them

--- test/unit/colors.spec.js
+++ test/unit/colors.spec.js
@@ -1,25 +1,22 @@
-import { describe, test, vi } from 'vitest'
+import { describe, suite, test, vi } from 'vitest'
 
-import { blue, bold, dim, red, yellow } from '../../lib/colors.js'
+suite('colors', () => {
+  describe('color functions', async () => {
+    vi.stubEnv('FORCE_COLOR', '1')
+    process.stdout.hasColors = vi.fn().mockReturnValue(true)
 
-const colors = {
-  red,
-  dim,
-  blue,
-  bold,
-  yellow,
-}
+    const { red, dim, blue, bold, yellow } = await import('../../lib/colors.js')
 
-vi.stubEnv('FORCE_COLOR', 'true')
+    const colors = { red, dim, blue, bold, yellow }
 
-describe('colors', () => {
-  test.for([
-    ['red', '\u001b[31m_\u001b[39m'],
-    ['dim', '\u001b[2m_\u001b[22m'],
-    ['blue', '\u001b[34m_\u001b[39m'],
-    ['bold', '\u001b[1m_\u001b[22m'],
-    ['yellow', '\u001b[33m_\u001b[39m'],
-  ])("should format '_' in $0 as $1", ([color, expected], { expect }) => {
-    expect(colors[color]('_')).toEqual(expected)
+    test.for([
+      ['red', '\u001b[31m_\u001b[39m'],
+      ['dim', '\u001b[2m_\u001b[22m'],
+      ['blue', '\u001b[34m_\u001b[39m'],
+      ['bold', '\u001b[1m_\u001b[22m'],
+      ['yellow', '\u001b[33m_\u001b[39m'],
+    ])("should format '_' in $0 as $1", ([color, expected], { expect }) => {
+      expect(colors[color]('_')).toBe(expected)
+    })
   })
 })
